15c5d003ef7198ad96959ebfa018103bdd54f9f9
saneenKP <saneenkp2000@gmail.com>
2022-04-14 23:32:01 +0530

    request model done

--- Models/requestModel.js
+++ Models/requestModel.js
@@ -1,7 +1,24 @@
-export var request = (req , url) => {
-    return{
-        url: url.parse(req.url , true),
-        method: req.method.toUpperCase(),
-        path: url.pathname.replace(/^\/+|\/+$/g,""),
-    }
+var request = (req , url) => {
+
+    var parsedUrl = url.parse(req.url , true)
+    var path= parsedUrl.pathname.replace(/^\/+|\/+$/g , "")
+    var method= req.method
+
+    if(parsedUrl == null || parsedUrl == undefined)
+        throw new Error("Invalid URL !")
+
+    if(path == null || path == undefined)
+        throw new Error("Invalid path or query string !")
+    
+    if(method != 'POST')
+        throw new Error("Invalid method !")
+
+    return {
+        Url:parsedUrl,
+        path: path,
+        method: method
+    };
+
 }
+
+module.exports = request
