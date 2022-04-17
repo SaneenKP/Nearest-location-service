e91848d27a32b7f54fa395464ca5a4186eccd2e2
saneenKP <saneenkp2000@gmail.com>
2022-04-17 12:43:58 +0530

    indexes edited

--- DB/indexes.js
+++ DB/indexes.js
@@ -7,7 +7,6 @@ const getIndex = (index) => {
         }
     }
 
-
     //Check if the indexing point is an undefined index.
     if(typeof indexes[index] == "undefined")
         throw new Error("Invalid indexing node !")
