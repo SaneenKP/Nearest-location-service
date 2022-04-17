670a1526a9460ab8cee111eeed85b65235693cce
saneenKP <saneenkp2000@gmail.com>
2022-04-17 15:02:51 +0530

    indexes and query moved into entities

---
+++ DB/Entities/query.js
@@ -0,0 +1,17 @@
+const getNearestCoordinatesQuery = (longitude , latitude , max , min) => {
+
+    return {
+        location: {
+            $near: {
+                $geometry: {
+                    type:"Point",
+                    coordinates: [longitude , latitude]
+                },
+                $maxDistance: max,
+                $minDistance: min,
+            }
+        }
+    }
+}
+
+module.exports = getNearestCoordinatesQuery
