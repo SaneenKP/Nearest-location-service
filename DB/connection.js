079eb260407942190969596a9b03dcbe6614e21d
saneenKP <saneenkp2000@gmail.com>
2022-04-28 05:29:55 +0530

    db connections framed correctyl

--- DB/connection.js
+++ DB/connection.js
@@ -4,36 +4,54 @@ const createIndex = require('./createIndex.js');
 
 //Indexing Query
 const indexQuery = require('./Entities/indexes.js');
-const index = indexQuery("location")
-
 
 //Database Constrains
 const databaseName = "sample_location"
 const collectionName = "UsersAndLocations"
 const uri = 'mongodb+srv://test:0kUkd360hKNaraDd@location-testing.mny1q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
+//variables
+const client = new MongoClient(uri)
+const indexQueryString = "location"
+
 const connect = async () => {
 
-    const client = new MongoClient(uri)
     try{
-
         await client.connect()
         console.log("MongoDB connected !");
 
-        const indexStatus = await createIndex(client , databaseName , collectionName , index) 
-        if(indexStatus != ""){
-            console.log("MongoDB index Created : " + indexStatus);
-        }
+        await setIndex()
 
     }catch(e){
-        console.log("Mongo Error: " + e);
-    }finally{
-
-        await client.close()
-        console.log("MongoDB client closed !");
+        throwError(e);
     }
     return client
 }
 
+const setIndex = async () => {
+    try{
+        const index = indexQuery(indexQueryString)
+        const indexStatus = await createIndex(client , databaseName , collectionName , index) 
+        if(indexStatus != ""){
+            console.log("MongoDB index Created : " + indexStatus);
+        }
+    }catch(e){
+        throwError(e)
+    }
+}
 
-module.exports = connect
+const close = async () => {
+
+    try{
+        await client.close();
+        console.log("MongoDB connection close !");
+    }catch(e){
+        throwError(e)
+    }
+}
+
+const throwError = (e) => {
+    throw new Error("Mongo Error : " + e)
+}
+
+module.exports = {connect , close}
